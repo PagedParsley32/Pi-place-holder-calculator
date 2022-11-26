c341e1a3bccf014fc7528e8d259dd629a5e43297
PagedParsley32 <119122813+PagedParsley32@users.noreply.github.com>
2022-11-26 15:57:16 +0000

    Add files via upload

---
+++ pi_placeholder_calculator.py
@@ -0,0 +1,19 @@
+place_holder = ()
+
+import os 
+import time
+import math
+
+def welcome_screen(place_holder):
+    print(f"Wellcome to my pi place holder calculator(by max m-d)")
+    print (f"Press [Enter] to contine")
+    input = ()
+    os.sytem("clear")
+    print(f"Plese select an intager within pi to calculate... ")
+    place_holder = int(input())
+    os.sytem("clear")
+    
+
+
+
+
